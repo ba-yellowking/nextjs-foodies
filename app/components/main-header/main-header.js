99f87311113e2ddc14c750eb5aa8dff1e72cbc5d
Bakytzhan Temirgali <ba.temirgali@gmail.com>
2025-06-20 13:14:25 +0500

    Add 'use client' component, add usePathname hook

--- app/components/main-header/main-header.js
+++ app/components/main-header/main-header.js
@@ -1,11 +1,12 @@
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
-
 import Link from "next/link";
 import Image from "next/image";
 import MainHeaderBackground from "@/app/components/main-header/main-header-background";
+import NavLink from "@/app/components/main-header/nav-link";
 
 export default function MainHeader() {
+
   return (
     <>
       <MainHeaderBackground/>
@@ -18,10 +19,18 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink
+                href="/meals"
+              >
+                Browse Meals
+              </NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink
+                href="/community"
+              >
+                Foodies Community
+              </NavLink>
             </li>
           </ul>
         </nav>
